33c7de3a0a27caf1db4013ff2c92e2f092f39e6b
box-jeon <utrpanic@gmail.com>
2021-03-04 11:26:15 +0900

    Temp

--- Gump/View/RootView.swift
+++ Gump/View/RootView.swift
@@ -2,10 +2,8 @@ import SwiftUI
 
 struct RootView: View {
     
-    @State private var selection: Set<LocalRepo> = [.brunch]
-    
     var sidebar: some View {
-        List(LocalRepo.allCases, id:\.rawValue,  selection: $selection) { localRepo in
+        List(LocalRepo.allCases, id:\.rawValue) { localRepo in
             NavigationLink(localRepo.rawValue, destination: BranchesView().frame(width: 320, height: 640))
         }
         .listStyle(SidebarListStyle())
